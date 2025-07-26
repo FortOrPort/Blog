45c630a2336003440c5ed576e31fb5753332e9a4
FortOrPort <patrick.smith9795@gmail.com>
2025-07-26 11:11:49 -0400

    Add scss

--- src/index.mjs
+++ src/index.mjs
@@ -4,14 +4,16 @@ import hbs from "hbs";
 import express from 'express';
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>{
-    console.log(`http://localhost:${PORT}`);
-})
+
+app.use(express.static('./public'));
 
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) =>{
     res.render('home');
+})
+
+app.listen(PORT, () =>{
+    console.log(`http://localhost:${PORT}`);
 })
